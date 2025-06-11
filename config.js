48caf3d92fd53978c9f39699e5091d7f2c64c52a
shriramlonikar <shriramlonikar618@gmail.com>
2025-06-11 18:30:25 +0530

    first commit

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb://localhost:27017/bookStore'
-
-// 'mongodb+srv://shriramlonikar618:shriramlonikar618@practice.rfw8g.mongodb.net/bookStore'
+export const mongoDBURL = 'mongodb+srv://shriramlonikar618:shriramlonikar25@mycluster.7gsyqvc.mongodb.net/bookStore'
